9d9ea8977c113d2f877d057bbf05020e2bcbfa53
Egor Aksenov <anonaso3011@gmail.com>
2025-08-11 21:47:00 +0300

    fix: add token

--- tgBot/main.py
+++ tgBot/main.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("")
+bot = telebot.TeleBot("8203033092:AAEhdT1Id69ImYWKA-VDuLVMKo76kyLW8HY")
 
 @bot.message_handler(commands=["start"])
 def send_start_mes(message):
