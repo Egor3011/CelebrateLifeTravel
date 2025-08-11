278ae44900c941a27cb1469e997c46a7fba5971d
Egor Aksenov <anonaso3011@gmail.com>
2025-08-11 21:49:41 +0300

    fix: polling tgbot

--- tgBot/main.py
+++ tgBot/main.py
@@ -5,3 +5,6 @@ bot = telebot.TeleBot("8203033092:AAEhdT1Id69ImYWKA-VDuLVMKo76kyLW8HY")
 @bot.message_handler(commands=["start"])
 def send_start_mes(message):
     bot.send_message(message.chat.id, "Привет - это набросок")
+
+
+bot.polling(none_stop=True)
