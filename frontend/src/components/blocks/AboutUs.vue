aaf39e01c6d77eae4b1b3b98b88fd3c9976bb7ad
Egor Aksenov <anonaso3011@gmail.com>
2025-08-13 18:14:11 +0300

    addTestBlocksAndGSAP

---
+++ frontend/src/components/blocks/AboutUs.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="aboutUs-Block">
+        <div>
+            <span class="headH1 komand--AboutUs">Команда</span>
+            <h2 class="titleBlock" style="width: 500px;">CELEBRATE LIFE TRAVEL  – это двигатель ваших приключений</h2>
+        </div>
+        <div style="text-align: center;">
+            <h2>Наша команда</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { onMounted } from "vue";
+
+gsap.registerPlugin(ScrollTrigger);
+
+onMounted(() => {
+    const tl = gsap.timeline()
+    tl.from(".komand--AboutUs", {
+        x: -100,
+        opacity: 0,
+        duration: 0.5
+    })
+    tl.from(".titleBlock", {
+        x: -100,
+        opacity: 0,
+        duration: 1
+    })
+})
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>АВТОРСКИЕ ТУРЫ</h1>
   </main>
 </template>
