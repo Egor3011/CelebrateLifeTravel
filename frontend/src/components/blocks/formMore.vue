6fc3b85250c3563f2502cd6d3370cd65d308be23
Egor Aksenov <anonaso3011@gmail.com>
2025-08-17 23:55:29 +0300

    fix: tel input

--- frontend/src/components/blocks/formMore.vue
+++ frontend/src/components/blocks/formMore.vue
@@ -6,7 +6,7 @@
                 <span>Заполните форму, и наш администратор в скором времени свяжется с вами, чтобы ответить на все вопросы</span>
                 <input type="text" placeholder="Ваше имя">
                 <input type="text" placeholder="yourmail@example.com">
-                <input type="text" placeholder="+ 7 (900) 900-90-90">
+                <input type="tel" placeholder="+ 7 (900) 900-90-90">
                 <button>Оставить заявку</button>
             </div>
             <div>
