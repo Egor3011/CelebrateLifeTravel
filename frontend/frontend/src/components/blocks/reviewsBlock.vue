6e62755543d6146d37b3ee23de1856971d61d47f
Egor Aksenov <anonaso3011@gmail.com>
2025-08-19 02:54:46 +0300

    test

--- frontend/frontend/src/components/blocks/reviewsBlock.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="reviewBlock container">
-        <div class="titleBlockDiv__reviewBlock">
-            <span class="headH1">Отзывы</span>
-            <h2 class="titleBlock" style="width: 500px;">Ваши отзывы о наших путешествиях</h2>
-        </div>
-    </div>
-</template>
--- frontend/src/components/blocks/FAQBlock.vue
+++ frontend/src/components/blocks/FAQBlock.vue
@@ -4,9 +4,9 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { ref } from "vue";
 import { onMounted } from "vue";
-import Acardion from "./Acardion.vue";
+import AccordionItem from "./AccordionItem.vue";
 
-const faqBlock = ref([{title: "kdskfjds", info: "ghbdtn nen nfrfz byaf"},{title: "kdskfjds", info: "ghbdtn nen nfrfz byaf"},{title: "kdskfjds", info: "ghbdtn nen nfrfz byaf"}])
+const faqBlock = ref([{title: "kdskfjds", info: "Или как говорят наши туристы - самый бомбовый гид Дагестана. И это действительно так, в одном человеке собраны все самые лучшие качества отличного гида: любовь к своему родному региону, искрометный юмор, тактичность, забота, внимание к каждому туристу, великолепное знание истории Дагестана."},{title: "kdskfjds", info: "ghbdtn nen nfrfz byaf"},{title: "kdskfjds", info: "ghbdtn nen nfrfz byaf"}])
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -33,7 +33,7 @@ onMounted(() => {
         </div>
 
         <div class="containerFAQ__blocks">
-            <Acardion
+            <AccordionItem
                 v-for="(el, index) in faqBlock"
                 :key="index"
                 :question="el.title"
@@ -57,4 +57,33 @@ onMounted(() => {
 
 
 /*тут классы для кнопок*/
+
+.FAQButton__container {
+    padding: 0px 40px;
+    margin: 30px;
+
+    align-content: center;
+
+    border-radius: 20px;
+    box-shadow: 0 0 10px var(--vt-shadow-color);
+
+    overflow: hidden;
+    transition: 1s;
+}
+
+.FAQButton__container:hover {
+    transition: 1s;
+
+    .FAQButton__mainText {
+        display:block;
+    }
+}
+
+.FAQButton__titleText {
+    margin: 5px 0;
+}
+
+.FAQButton__mainText:not(.active) {
+    display: none;
+}
 </style>
