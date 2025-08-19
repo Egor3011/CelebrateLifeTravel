1905ebda0bf23417862d654da20014c535ff848b
Egor Aksenov <anonaso3011@gmail.com>
2025-08-19 23:55:56 +0300

    fix text

--- frontend/src/components/TourDatesBlock.vue
+++ frontend/src/components/TourDatesBlock.vue
@@ -3,7 +3,7 @@
     <h2 class="block-title">Даты туров</h2>
     <div class="dates-grid">
       <div class="date-column" v-for="(monthData, monthName) in tourDates" :key="monthName">
-        <span class="month-name">{{ monthName }}</span>
+        <h3 class="month-name">{{ monthName }}</h3>
         <div class="days-list">
           <span 
             v-for="(days, index) in monthData"
