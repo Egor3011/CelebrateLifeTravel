eb962e2f9e7422be3512938281804e83af0b6647
Egor Aksenov <anonaso3011@gmail.com>
2025-08-29 00:54:17 +0300

    fix

--- tgBot/main.py
+++ tgBot/main.py
@@ -59,7 +59,7 @@ def ask_tour_name(message):
     markupTour = types.ReplyKeyboardMarkup() # Replace with actual manager username
     for i in cfg.TOURS:
         markupTour.add(types.KeyboardButton("Связаться с менеджером"))
-    bot.send_message(message.chat.id, "И последнее: как называется тур, который тебя интересует?")
+    bot.send_message(message.chat.id, "И последнее: как называется тур, который тебя интересует?", reply_markup=markupTour)
     bot.register_next_step_handler(message, show_contact_button)
 
 
