55b1d2739bdd5f2cf768bfd9ee82db55bc979a99
Egor Aksenov <anonaso3011@gmail.com>
2025-08-29 01:03:21 +0300

    add dogovor

--- backend/documents.py
+++ backend/documents.py
@@ -17,8 +17,16 @@ async def get_oferta_pdf():
     return FileResponse(file_path, media_type="application/pdf")
 
 
+@documents.get("/dogovor")
+async def get_dogovor_pdf():
+    file_path = "./documents/dogovor_test.pdf"
+    if not os.path.exists(file_path):
+        raise fastapi.HTTPException(status_code=404, detail="Dogovor PDF not found")
+    return FileResponse(file_path, media_type="application/pdf")
+
+
 @documents.get("/politika_konf")
-async def get_oferta_pdf():
+async def get_politika_pdf():
     file_path = "./documents/politika_konf.pdf"
     if not os.path.exists(file_path):
         raise fastapi.HTTPException(status_code=404, detail="Politika_konf PDF not found")
@@ -26,7 +34,7 @@ async def get_oferta_pdf():
 
 
 @documents.get("/skan_ip")
-async def get_oferta_pdf():
+async def get_skanIP_pdf():
     file_path = "./documents/skan_IP.pdf"
     if not os.path.exists(file_path):
         raise fastapi.HTTPException(status_code=404, detail="Skan_IP PDF not found")
