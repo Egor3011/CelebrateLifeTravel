4022c69835f0e7b0b667244167b5cafdf8346805
Egor Aksenov <anonaso3011@gmail.com>
2026-09-02 12:32:59 +0300

    close documents

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -17,11 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
-    {
-      path: '/documents',
-      name: 'documents',
-      component: () => import('../views/OfertaView.vue'),
-    },
+    //{
+    //  path: '/documents',
+    //  name: 'documents',
+    //  component: () => import('../views/OfertaView.vue'),
+    //},
   ],
 })
 
